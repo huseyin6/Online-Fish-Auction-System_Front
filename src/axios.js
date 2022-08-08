6bb623498033c46d0927477f50f63abacab2b3f0
huseyin6 <100037752+huseyin6@users.noreply.github.com>
2022-08-08 15:12:30 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { api } from './urlconfig';
 const token = window.localStorage.getItem('token');
+
 const axiosInstance = axios.create({
   baseURL: api,
   headers: {
